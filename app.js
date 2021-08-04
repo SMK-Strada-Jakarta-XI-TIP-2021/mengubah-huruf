84bf0708e12099217d2a4dd81353706af0e52005
maxdesanta <maxdesanta1@gmail.com>
2021-08-04 22:27:50 +0700

    tugas mengubah huruf max

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // Tulis kode kamu di bawah ini
+// bahan 
+let kalimat = 'i love javascript';
+let perubahanKalimat = '';
+
+//for tanpa of
+// for (let a = 0; a <= kalimat.length; a++) {
+//     if (kalimat[a] === 'a' || kalimat[a] === 'i' ||kalimat[a] === 'u' || kalimat[a] === 'e' || kalimat[a] === 'o') {
+//         kalimat[a] = '$';
+//     }
+//     perubahanKalimat = + kalimat[a];
+// }
+
+// console.log("kalimat sudah berubah menjadi : " + perubahanKalimat);
+
+// pake for of
+for (x of kalimat) {
+    if( x === 'a' || x === 'i' || x === 'u' || x === 'e' || x === 'o'){
+    	x = '$';
+    }
+  perubahanKalimat += x;
+}
+
+console.log("kalimat sudah berubah menjadi : " + perubahanKalimat)
