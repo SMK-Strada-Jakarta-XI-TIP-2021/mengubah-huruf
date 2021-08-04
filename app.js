b2ebd36afc67d4e264b0591e30e2b01e945d1dac
myairin <irenewindagultom@gmail.com>
2021-08-05 05:18:46 +0700

    tugas ketiga sudah selesai, maaf atas keterlambatanya

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // Tulis kode kamu di bawah ini
+let kalimat = 'i love javascript';
+let perubahanKalimat = '';
+for (x of kalimat) {
+    if( x === 'a' || x === 'i' || x === 'u' || x === 'e' || x === 'o'){
+    	x = '$';
+    }
+  perubahanKalimat += x;
+}
+console.log('i love javascript')
+console.log("kalimat akan berubah menjadi : " + perubahanKalimat)
