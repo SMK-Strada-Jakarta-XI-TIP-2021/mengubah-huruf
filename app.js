a6374e8d645124698d7a98755fccb114af3a8edd
adrigunawan <adrigunawan32@gmail.com>
2021-08-06 00:34:20 +0700

    Tugas mengubah huruf

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // Tulis kode kamu di bawah ini
+var kalimat = 'i love javascript';
+var hasil = '';
+for (i of kalimat) {
+    if ( i === 'a' || i === 'i' || i === 'u' || i === 'e' || i === 'o'){
+    	i = '$';
+    }
+  hasil += i;
+}
+
+console.log('Kalimat berubah menjadi' + ' = ' + hasil)
