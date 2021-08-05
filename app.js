7ee83df39158825e8fa270c035c40aeeed008802
unknown <dwitegar611@gmail.com>
2021-08-05 19:37:41 +0700

    mengubah huruf

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // Tulis kode kamu di bawah ini
+let kalimat = 'i love javascript';
+let hasilKalimat = '';
+
+for (x of kalimat) {
+    if ( x === 'a' || x === 'i' || x === 'u' || x === 'e' ||x === 'o' )
+    {
+        x = '$';
+    }
+    hasilKalimat += x;
+}
+
+console.log("kalimat sudah berubah menjadi : " + hasilKalimat)
