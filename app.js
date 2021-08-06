b63712c5c98774e1ae2d23a4162cf261f35cc5d9
limmi26 <kisamakiazuel@gmail.com>
2021-08-06 19:02:26 +0700

    perubahan 1

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // Tulis kode kamu di bawah ini
+
+var kalimat = 'i love javasript';
+var hasil = '';
+
+for (x of kalimat) {
+  if (x === 'a' || x === 'i' || x === 'u' || x === 'e' || x === 'o') {
+    x = '$';
+  }
+  hasil += x;
+}
+
+console.log("maka hasil yang dihasilkan adalah " + hasil);
