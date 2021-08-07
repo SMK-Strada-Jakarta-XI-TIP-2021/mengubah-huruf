bb32bc63b597a0d23fb9f630d77417988b5ee9c6
sulthanarif <msarizky201@gmail.com>
2021-08-07 15:04:12 +0700

    mengubah huruf sulthan

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // Tulis kode kamu di bawah ini
+
+var kalimat = 'i love javascript';
+var kalimatBaru='';
+
+for (x of kalimat) {
+    if( x === 'a' || x === 'i' || x === 'u' || x === 'e' || x === 'o'){
+    	x = '$';
+    }
+    kalimatBaru+= x
+    
+
+}
+console.log(kalimatBaru)
