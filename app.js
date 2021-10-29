21239686a06581107101375be7444bcbf088f58b
zkarine <zefanyakarine03@gmail.com>
2021-10-29 15:01:49 -0700

    mengubah-huruf zeff

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // Tulis kode kamu di bawah ini
+
+var kalimat = 'i love javascript';
+var kalimatDiubah = '';
+for (a of kalimat) {
+    if ( a === 'a' || a === 'i' || a === 'u' || a === 'e' || a === 'o'){
+    	a = '$';
+    }
+  kalimatDiubah += a;
+}
+
+console.log(kalimatDiubah);
